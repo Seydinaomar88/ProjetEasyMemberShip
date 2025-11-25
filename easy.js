2a896b9972cfcb3258833f9d3589a97ec7198e33
bayeabdu10 <abdoulahimbow29@gmail.com>
2025-11-25 11:44:00 +0000

    page accueil activites

---
+++ easy.js
@@ -0,0 +1,53 @@
+
+window.addEventListener('scroll', () =>{
+    const sec1 = document.querySelector('#sec1')
+    if(window.scrollY > 400){
+        sec1.style.display = 'flex'
+    }else{
+        sec1.style.display = 'none'
+    }
+
+})
+window.addEventListener('scroll',() => {
+    const sec2 = document.querySelector('#sec2')
+    if(window.scrollY > 800){
+        sec2.style.display = 'flex'
+    } else{
+        sec2.style.display = 'none'
+    }
+})
+
+window.addEventListener('scroll', () =>{
+    const sec3 = document.querySelector('#sec3')
+    if(window.scrollY > 800){
+        sec3.style.display = 'flex'
+    } else{
+        sec3.style.display = 'none'
+    }
+})
+window.addEventListener('scroll', () =>{
+    const sec4 = document.querySelector('#sec4')
+    if(window.scrollY > 1500){
+        sec4.style.display = 'flex'
+    } else{
+        sec4.style.display = 'none'
+    }
+})
+
+window.addEventListener('scroll', () =>{
+    const sec5 = document.querySelector('#sec5')
+    if(window.scrollY > 2300){
+        sec5.style.display = 'flex'
+    } else {
+        sec5.style.display = 'none'
+    }
+})
+
+window.addEventListener('scroll', () =>{
+    const sec6 = document.querySelector('#sec6')
+    if(window.scrollY > 2600){
+        sec6.style.display = 'flex'
+    } else{
+        sec6.style.display = 'none'
+    }
+})
